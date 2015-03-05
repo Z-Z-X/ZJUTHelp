6144be03c7cd3bf3d91c77a0fa6ce8351fe3bd99
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-03-05 13:55:01 +0800

    实现菜单选中效果

--- app/src/main/java/com/zjut/zjuthelp/Fragments/SettingsFragment.java
+++ app/src/main/java/com/zjut/zjuthelp/Fragments/SettingsFragment.java
@@ -13,12 +13,12 @@ import com.zjut.zjuthelp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link NewsFragment.OnFragmentInteractionListener} interface
+ * {@link SettingsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link NewsFragment#newInstance} factory method to
+ * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewsFragment extends Fragment {
+public class SettingsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -36,11 +36,11 @@ public class NewsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFragment.
+     * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NewsFragment newInstance(String param1, String param2) {
-        NewsFragment fragment = new NewsFragment();
+    public static SettingsFragment newInstance(String param1, String param2) {
+        SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -48,7 +48,7 @@ public class NewsFragment extends Fragment {
         return fragment;
     }
 
-    public NewsFragment() {
+    public SettingsFragment() {
         // Required empty public constructor
     }
 
@@ -65,7 +65,7 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/zjut/zjuthelp/MainActivity.java
+++ app/src/main/java/com/zjut/zjuthelp/MainActivity.java
@@ -1,39 +1,122 @@
 package com.zjut.zjuthelp;
 
-import android.graphics.Color;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends ActionBarActivity {
+import com.zjut.zjuthelp.Fragments.LibraryFragment;
+import com.zjut.zjuthelp.Fragments.NewsFragment;
+import com.zjut.zjuthelp.Fragments.SettingsFragment;
+import com.zjut.zjuthelp.Fragments.TeachingAffairsFragment;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+
+    private FragmentManager fragmentManager;
+
+    //主菜单控件
+    private LinearLayout itemNews;
+    private LinearLayout itemLibrary;
+    private LinearLayout itemTeachingAffairs;
+    private LinearLayout itemSettings;
+
+    //页面
+    private NewsFragment newsFragment;
+    private LibraryFragment libraryFragment;
+    private TeachingAffairsFragment teachingAffairsFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //初始化工具栏
         toolbar = (Toolbar)findViewById(R.id.main_toolbar);
+        //初始化侧滑菜单
         drawerLayout = (DrawerLayout)findViewById(R.id.main_drawer);
+        //初始化菜单中的项目
+        itemNews = (LinearLayout)findViewById(R.id.item_news);
+        itemLibrary = (LinearLayout)findViewById(R.id.item_library);
+        itemTeachingAffairs = (LinearLayout)findViewById(R.id.item_teaching_affairs);
+        itemSettings = (LinearLayout)findViewById(R.id.item_settings);
+        //事件处理
+        itemNews.setOnClickListener(this);
+        itemLibrary.setOnClickListener(this);
+        itemTeachingAffairs.setOnClickListener(this);
+        itemSettings.setOnClickListener(this);
 
-        toolbar.setTitleTextColor(Color.WHITE);
+        fragmentManager = getFragmentManager();
+
+        //配置工具栏
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //开启关闭菜单
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close) {
+            String appTitle;
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                appTitle = getTitle().toString();
+                setTitle(getResources().getString(R.string.app_name));
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                setTitle(appTitle);
+            }
+        };
         drawerToggle.syncState();
         drawerLayout.setDrawerListener(drawerToggle);
 
-        //载入页面
-
+        //Load News Fragment
+        onClick(itemNews);
     }
 
+    //事件处理
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        resetMenu();
+        switch (v.getId()) {
+            case R.id.item_news:
+                setTitle(R.string.news);
+                itemNews.setBackgroundColor(getResources().getColor(R.color.colorLight));
+                break;
+            case R.id.item_library:
+                setTitle(R.string.library);
+                itemLibrary.setBackgroundColor(getResources().getColor(R.color.colorLight));
+                break;
+            case R.id.item_teaching_affairs:
+                setTitle(R.string.teaching_affairs);
+                itemTeachingAffairs.setBackgroundColor(getResources().getColor(R.color.colorLight));
+                break;
+            case R.id.item_settings:
+                setTitle(R.string.settings);
+                itemSettings.setBackgroundColor(getResources().getColor(R.color.colorLight));
+                break;
+        }
+        drawerLayout.closeDrawers();
+    }
+
+    //Reset Menu
+    private void resetMenu() {
+        itemNews.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+        itemLibrary.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+        itemTeachingAffairs.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+        itemSettings.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+    }
+
+    //Hide Fragement
 }
